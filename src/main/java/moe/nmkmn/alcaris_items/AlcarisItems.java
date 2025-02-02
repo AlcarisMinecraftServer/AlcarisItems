c7779b29ba23c3cfbbeda2474298e24a2684dcd2
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-02-02 19:13:00 +0900

    feat: initial commit

---
+++ src/main/java/moe/nmkmn/alcaris_items/AlcarisItems.java
@@ -0,0 +1,17 @@
+package moe.nmkmn.alcaris_items;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AlcarisItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
